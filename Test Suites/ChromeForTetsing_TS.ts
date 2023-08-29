cbd703e07c92dd90a262d871d321dbbc9a80e850
linh.nguyen@katalon.com <linh.nguyen@katalon.com>
2023-08-29 19:42:15 +0700

    commit TS

---
+++ Test Suites/ChromeForTetsing_TS.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ChromeForTetsing_TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5981ae61-8400-4bb9-9cb6-3d37d7fdd9de</testSuiteGuid>
+</TestSuiteEntity>
